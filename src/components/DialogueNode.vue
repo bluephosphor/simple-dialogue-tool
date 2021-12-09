637a73d445ab80a76b13fe949ec61e595b881a1e
Niko <bow682@gmail.com>
2021-12-09 11:25:15 -0500

    ok

--- src/components/DialogueNode.vue
+++ src/components/DialogueNode.vue
@@ -1,67 +1,46 @@
 <script>
+import NodeAttribute from "./NodeAttribute"
 import Button from "./Button";
 
 export default {
   name: "DialogueNode",
   components: {
-    Button,
+    NodeAttribute,
+    Button
   },
   props: {
     nodeData: Object,
   },
   data() {
     return {
-      editing: false,
-      type: this.nodeData.type,
-      speaker: this.nodeData.speaker,
-      text: this.nodeData.text,
-      event: this.nodeData.event,
+      editing:  false,
+      type:     this.nodeData.type,
+      speaker:  this.nodeData.speaker,
+      text:     this.nodeData.text,
+      event:    this.nodeData.event,
     };
   },
-  methods: {
-    toggleEdit() {
-      this.editing = !this.editing;
-    },
-  },
+  methods:{
+      toggleEdit(){
+          this.editing = !this.editing
+      }
+  }
 };
 </script>
 
 <template>
-  <div class="card">
-    <div class="card-body">
-      <p class="label">speaker:</p>
-      <input
-        v-if="this.editing"
-        v-model="this.speaker"
-        class="text-input"
-        placeholder="text"
-      />
-      <p v-else class="card-text">{{ this.speaker }}</p>
-
-      <p class="label">text:</p>
-      <input
-        v-if="this.editing"
-        v-model="this.text"
-        class="text-input"
-        placeholder="text"
-      />
-      <p v-else class="card-text">{{ this.text }}</p>
-
-      <p class="label">event:</p>
-      <input
-        v-if="this.editing"
-        v-model="this.event"
-        class="text-input"
-        placeholder="text"
-      />
-      <p v-else class="card-text">{{ this.event }}</p>
-    </div>
+  <table class="card">
+    <tbody class="card-body">
+      <NodeAttribute name="speaker" :attribute="this.speaker"   :editing="this.editing"/>
+      <NodeAttribute name="text"    :attribute="this.text"      :editing="this.editing"/>
+      <NodeAttribute name="event"   :attribute="this.event"     :editing="this.editing"/>
+    </tbody>
     <Button
       :text="this.editing ? 'Save' : 'Edit'"
       color="blue"
       @click="toggleEdit"
     />
-  </div>
+  </table>
 </template>
 
 <style scoped>
@@ -74,23 +53,5 @@ export default {
   padding: 8px;
 }
 
-.card-body{
-    display: grid;
-  grid-template-rows: auto auto;
-  grid-template-columns: auto auto;
-}
 
-.attribute {
-  display: flex;
-}
-
-.text-input {
-  font-family: monospace;
-  background: black;
-  color: white;
-  border: 1px solid white;
-  padding: 4px;
-  margin: 8px 0px;
-  width: 90%;
-}
 </style>
---
+++ src/components/NodeAttribute.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  props: {
+    name: String,
+    attribute: String,
+    editing: Boolean,
+  },
+  data() {
+    return {
+      value: this.attribute,
+    };
+  },
+};
+</script>
+<template>
+  <tr class="attribute">
+    <td class="label">{{ this.name }}:</td>
+    <td v-if="this.editing">
+      <input v-model="this.value" class="text-input" :placeholder="this.name" />
+    </td>
+    <td v-else class="card-text">{{ this.value }}</td>
+  </tr>
+</template>
+
+<style scoped>
+
+.text-input {
+  font-family: monospace;
+  background: black;
+  color: white;
+  border: 1px solid white;
+  width: 98%;
+  height: 100%
+}
+
+td{
+    padding: 4px;
+    margin: 16px 0px;
+    height: 24px;
+}
+
+.label{
+    width:20%;
+}
+</style>
